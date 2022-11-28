63223396a7a06ab837a8d7eb1863fdd5dfc129ab
Victor <victoralexsander5200@gmail.com>
2022-11-28 20:51:16 -0300

    att

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 "use strict";
 import * as http from 'http';
 import { app } from './app';
+import * as mongoose from "mongoose";
+
 
 const server = http.createServer(app).listen(parseInt(process.env.PORT || '5000', 10));
 // CONEXAO COM MONGOOSE VEM AQUI
+
+//Senha 45YIXN9I4cVkUWgC
+//170.83.162.217/32 My IP Address
+
+mongoose.connect('mongodb+srv://proleads:45YIXN9I4cVkUWgC@proleadscluster.v3t02mb.mongodb.net/test')
+.then(() => {
+    console.log("conectamos ao mongodb")
+})
+
+
